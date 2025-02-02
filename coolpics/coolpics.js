66c9f9da635d9ac5dcb79b1ba8403f46794a4fbd
Kaleb R Trogdon <67351277+KalebTrogdon@users.noreply.github.com>
2025-02-01 17:51:25 -0700

    Updated committed

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,9 +1,44 @@
-// JavaScript to handle the mobile menu
 document.addEventListener("DOMContentLoaded", () => {
     const menuButton = document.querySelector(".menu-button");
     const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.add("hide");
 
     menuButton.addEventListener("click", () => {
         navLinks.style.display = navLinks.style.display === "block" ? "none" : "block";
     });
+
+function handleResize() {
+    if (window.innerWidth > 1000) {
+        navLinks.classList.remove("hide"); 
+    } else {
+        navLinks.classList.add("hide"); 
+    }
+}
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
 });
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+            <button class="close-viewer">X</button>
+            <img src="${pic}" alt="${alt}">
+          </div>`;
+}
+
+function viewHandler(event) {
+    if (event.target.tagName === "IMG") {
+        let imgSrc = event.target.src.replace("norris-sm.jpeg", "norris-full.jpeg");
+        const imgAlt = event.target.alt;
+
+        document.body.insertAdjacentHTML("afterbegin", viewerTemplate(imgSrc, imgAlt));
+
+        document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+    }
+}
+
+function closeViewer() {
+    document.querySelector(".viewer").remove();
+}
+
+document.querySelector(".gallery").addEventListener("click", viewHandler);
